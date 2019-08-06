bd7a8ce7373be3c59f2118542c61152cfb6f4cac
Elena Welsh <elenawelsh@outlook.com>
2019-08-06 19:47:16 -0400

    third hello added

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -3,6 +3,7 @@ public class LoginTests{
     public static void main(String[] args) {
         System.out.println("line 1");
          System.out.println("line 2");
+        System.out.println("line 3");
     }
 
 
