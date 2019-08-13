865dc668f1462c59d70c19f8c2c070012d7db0e0
Elena Welsh <elenawelsh@outlook.com>
2019-08-13 19:43:56 -0400

    'changed'

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -4,6 +4,12 @@ public class LoginTests{
         System.out.println("line 1");
          System.out.println("line 2");
         System.out.println("line 3");
+        System.out.println("line 1");
+        System.out.println("line 2");
+        System.out.println("line 3");
+        System.out.println("line 1");
+        System.out.println("line 2");
+        System.out.println("line 3");
     }
 
 
